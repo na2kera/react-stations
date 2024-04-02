2699fb8519127a66853bae8cb7ad9e9df45fd595
na2kera <nat3mitaro@gmail.com>
2024-04-03 00:30:20 +0900

    station4

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 // DO NOT DELETE
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
+  )
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです</p>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" />
+      <img src={dogUrl} />
     </div>
   )
 }
