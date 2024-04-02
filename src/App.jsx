9621008ae24a6288ad110784287b8a83bd4f4f6d
na2kera <nat3mitaro@gmail.com>
2024-04-03 00:36:24 +0900

    onClickにそのままアロー関数で入れられる

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,19 @@ export const App = () => {
   const [dogUrl, setDogUrl] = useState(
     'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
   )
+
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです</p>
       <img src={dogUrl} />
+      <button
+        onClick={() => {
+          setDogUrl(
+            'https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg ',
+          )
+        }}
+      ></button>
     </div>
   )
 }
