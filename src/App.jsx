319873ec27948f5af219fef55012c5b634bc7120
na2kera <nat3mitaro@gmail.com>
2024-04-02 23:43:46 +0900

    station3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // DO NOT DELETE
-
+import React from 'react'
 import './App.css'
 
 /**
@@ -9,6 +9,8 @@ export const App = () => {
   return (
     <div>
       <header>Dogアプリ</header>
+      <p>犬の画像を表示するサイトです</p>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" />
     </div>
   )
 }
