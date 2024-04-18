b5221235c00c13823264f29c969492954a6860e5
na2kera <nat3mitaro@gmail.com>
2024-04-18 12:16:28 +0900

    asyncはコンポーネントには使えない

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,20 @@ import './App.css'
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState(
-    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
-  )
+  const [dogUrl, setDogUrl] = useState('')
+
+  async function onChange() {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random')
+    const images = await response.json()
+    setDogUrl(images.message)
+  }
 
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです</p>
       <img src={dogUrl} />
-      <button
-        onClick={() => {
-          setDogUrl(
-            'https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg ',
-          )
-        }}
-      ></button>
+      <button onClick={onChange}>変更</button>
     </div>
   )
 }
