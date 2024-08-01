8a914f016c7c790abba4e919387031da48e519aa
na2kera <nat3mitaro@gmail.com>
2024-08-01 11:04:40 +0900

    任意の犬の写真を表示

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,19 @@
 // @ts-check
 
-export const BreedsSelect = () => {
-  return <></>
+// @ts-ignore
+export const BreedsSelect = ({ breeds,setSelectedBreed}) => {
+
+
+  function handleChange(event){
+    setSelectedBreed(event.target.value)
+  }
+  return (
+    <>
+      <select onChange={handleChange}>
+        { Object.keys(breeds).map((breed) => <option value={breed} key={breed} >{breed}</option>)}
+      </select>
+    </>
+  )
 }
 
 export default BreedsSelect
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,22 +1,39 @@
 // @ts-check
 
 import { useEffect, useState } from 'react'
+import BreedsSelect from './BreedsSelect'
 
 export const DogListContainer = () => {
   const [breeds, setBreeds] = useState([])
+  const [selectedBreed, setSelectedBreed] = useState('')
+  const [dataList, setDataList] = useState([])
 
   useEffect(() => {
     const dataFetch = async () => {
       const res = await fetch('https://dog.ceo/api/breeds/list/all')
       const data = await res.json()
-      console.log(data.message)
       setBreeds(data.message)
     }
 
     dataFetch()
   }, [])
 
-  return <></>
+  const dogDataFetch= async() =>{
+    console.log({selectedBreed})
+    const res = await fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random/12`)
+    const data = await res.json()
+    console.log(res)
+    console.log(data.message)
+    setDataList(data.message)
+  }
+
+  return (
+    <>
+      <BreedsSelect breeds={breeds} setSelectedBreed={setSelectedBreed}/>
+      <button onClick={dogDataFetch}>表示</button>
+      {dataList.map((data)=><img src={data}/>)}
+    </>
+  )
 }
 
 export default DogListContainer
